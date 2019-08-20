67bc06c1def32451a9a0f3615adb9d955ea810ae
dsy <dsy@DESKTOP-FUGQT5S>
2019-08-20 10:18:18 +0800

    .

--- src/com/zzxx/music/service/SongService.java
+++ src/com/zzxx/music/service/SongService.java
@@ -2,11 +2,11 @@ package com.zzxx.music.service;
 
 import java.util.List;
 
-import com.zzxx.music.service.impl.Song;
+import com.zzxx.music.beans.Song;
 import com.zzxx.music.utils.PageBean;
 
 public interface SongService {
 	List<Song> listHotSong(int num);
-	PageBean<Song> listSongsByTppe(String currentPageStr, String pageSizeStr, String type);
+	PageBean<Song> listSongsByType(String currentPageStr, String pageSizeStr, String type);
 	
 }
